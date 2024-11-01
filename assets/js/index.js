95c3ce064d92a26425ddd6e4efd7108b160cfa41
Myles Reid <myles@reidsmailbox.com>
2024-11-01 12:02:45 -0500

    Styled

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,26 @@
 'use strict';
 
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+function listen(event, element, callback) {
+  return element.addEventListener(event, callback);
+}
+
+const colourSelect = select('#colour');
+
+listen('focus', colourSelect, () => {
+  colourSelect.size = 5;
+  colourSelect.style.position = 'absolute'
+});
+
+listen('blur', colourSelect, () => {
+  colourSelect.size = 0;
+  colourSelect.style.position = 'relative'
+});
+
+listen('change', colourSelect, () => {
+  colourSelect.size = 1;
+  colourSelect.blur();
+})
